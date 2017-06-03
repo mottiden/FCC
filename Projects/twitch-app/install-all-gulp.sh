c1228db7b34abd4653a84b91fc5ab41f91567044
Jacopo Parvizi <jacopo.parvizi@gmail.com>
2017-06-03 17:56:57 +0200

    Added Twitch App and Simon

---
+++ Projects/twitch-app/install-all-gulp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo Installing npm local repository
+npm init
+npm install gulp --save-dev
+npm install browser-sync babel-preset-es2015 --save-dev
+npm install gulp-autoprefixer --save-dev
+npm install gulp-useref --save-dev
+npm install gulp-uglify --save-dev
+npm install gulp-babel useref --save-dev
+npm install gulp-clean-css --save-dev
+npm install gulp-rename --save-dev
+npm install pump --save-dev
+npm install del --save-dev
---
+++ Projects/twitch-app/twitch-app.js
@@ -0,0 +1,6 @@
+// ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+
+
+// Use https://wind-bow.gomix.me/twitch-api instead of twitch's API base URL (i.e. https://api.twitch.tv/kraken )
+// https://wind-bow.gomix.me/twitch-api/streams/${user.name}
+// https://wind-bow.gomix.me/twitch-api/users/${user}
